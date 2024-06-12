34ae690335c7684b68d8b79c0cee8f0f2a382897
higorrb <higorbarbist16@gmail.com>
2024-06-12 16:04:22 -0300

    modified:   main.c 	new file:   output/abr_2024.txt 	new file:   output/fev_2024.txt 	modified:   output/main.exe 	new file:   output/mar_2024.txt

--- main.c
+++ main.c
@@ -9,8 +9,9 @@ typedef struct{
     char nome_arquivo[30];
 }dados;
 
+//ADICIONAR DADOS
 
-int Adicionar_dados(dados *reg, int *indice_registros){
+int Adicionar_dados(dados *reg, int *indice_registros, double *soma_mensal){
 
 
     FILE *arquivo = fopen(reg->nome_arquivo, "r");
@@ -24,46 +25,58 @@ int Adicionar_dados(dados *reg, int *indice_registros){
     while(fgets(linha, sizeof(linha), arquivo) != NULL){
 
 
-        sscanf(linha,"%2li/%li/%4li %lf", &reg[*indice_registros].dia, &reg[*indice_registros].mes, &reg[*indice_registros].ano, &reg[*indice_registros].litragem);
+        sscanf(linha,"%2li/%2li/%4li %lf", &reg[*indice_registros].dia, &reg[*indice_registros].mes, &reg[*indice_registros].ano, &reg[*indice_registros].litragem);
                
         printf("Dia: %li\n", reg[*indice_registros].dia);
         printf("Mes: %li\n", reg[*indice_registros].mes);
         printf("Ano: %li\n", reg[*indice_registros].ano);
         printf("Litragem: %.2f\n", reg[*indice_registros].litragem);
 
+        *soma_mensal += reg[*indice_registros].litragem;
 
     (*indice_registros)++;
     }
-   
+    
+    
+    
     fclose(arquivo);
     return 0;
 }
 
-int Dados_para_csv(dados *reg, int *indice_registros){
+//DADOS PARA CSV
+
+int Dados_para_csv(dados *reg, int indice_registros, double soma_mensal){
     
     int i;
     
-    FILE *arquivo = fopen("ListaDados.csv", "w");
+    FILE *arquivo = fopen("ListaDados.csv", "a");
         if(arquivo == NULL){
             printf("Erro ao abrir o arquivo");
             return 1;}
     
-    fprintf(arquivo,"DIA;MES;ANO;LITRAGEM\n");
-    
-    for(i = 0; i < *indice_registros; i++){
-        fprintf(arquivo, "%li;%li;%li;%.2f\n", reg[i].dia,reg[i].mes,reg[i].ano,reg[i].litragem);
+    fseek(arquivo, 0, SEEK_END);
+    long tamanho = ftell(arquivo);
+    if (tamanho == 0) {
+        fprintf(arquivo, "DIA;MES;ANO;LITRAGEM\n");
     }
     
+    for(i = 0; i < indice_registros; i++){
+        fprintf(arquivo, "%li;%li;%li;%.2f\n", reg[i].dia,reg[i].mes,reg[i].ano,reg[i].litragem);
+    }
+    fprintf(arquivo, "SOMA;MENSAL;%.2f\n\n", soma_mensal);
+
     fclose(arquivo);
     return 0;
     
 }
 
+//MAIN
+
 int main(int argc, char *argv[])
 {
-    dados reg[1000];
+    dados reg[31];
     int indice_registros = 0;
-
+    double soma_mensal = 0.0;
 
 
 
@@ -78,8 +91,13 @@ int main(int argc, char *argv[])
 
 
    
-    Adicionar_dados(reg, &indice_registros);
-    Dados_para_csv(reg, &indice_registros);
+    for (int i = 1; i < argc; i++) {
+        Adicionar_dados(reg, &indice_registros, &soma_mensal);
+    }
+
+    if (Dados_para_csv(reg, indice_registros, soma_mensal) != 0) {
+        return 1;
+    }
 
 
    
